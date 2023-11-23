49e3267a8741fc712d16fe5153bd75382bc4ae62
Nwankwo Henry <nameisifunanya@gmail.com>
2023-11-23 16:34:53 +0100

    Configured the DB connection

---
+++ config/dbConnection.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+let isConnected = false;
+
+const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (isConnected) {
+    console.log('MongoDB is Already Connected!');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, { dbName: 'inventoma' });
+
+    isConnected = true;
+    console.log('MONGODB IS CONNECTED!');
+  } catch (err) {
+    console.log('DB Connection Error: ', err);
+  }
+};
+
+exports.connectToDB;
