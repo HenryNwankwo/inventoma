2a2b059c64b0da88a44d04913083b468beef9dc0
Nwankwo Henry <nameisifunanya@gmail.com>
2023-11-23 16:24:40 +0100

    Created constants for some error codes

---
+++ config/constants.js
@@ -0,0 +1,10 @@
+const constants = {
+  BAD_REQUEST_ERROR: 400,
+  UNAUTHORIZED_ERROR: 401,
+  FORBIDDEN_ERROR: 403,
+  NOT_FOUND_ERROR: 404,
+  CONFLICT_ERROR: 409,
+  SERVER_ERROR: 500,
+};
+
+exports.constants;
